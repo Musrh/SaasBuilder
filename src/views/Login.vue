e189a50201d034e4f4ba78207589fd9cf9c963cf
Musrh <musplay2023@gmail.com>
2026-03-25 23:07:15 +0100

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,36 @@
 <template>
-  <div>
-    <h1>Login</h1>
+  <div class="container">
+    <h1>Connexion</h1>
+    <form @submit.prevent="loginUser">
+      <input v-model="email" type="email" placeholder="Email" required />
+      <input v-model="password" type="password" placeholder="Mot de passe" required />
+      <button type="submit">Se connecter</button>
+    </form>
   </div>
 </template>
 
 <script>
+import { auth } from "../firebase.js";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
 export default {
   name: "Login",
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    async loginUser() {
+      try {
+        await signInWithEmailAndPassword(auth, this.email, this.password);
+        alert("Connexion réussie !");
+        this.$router.push("/dashboard");
+      } catch (err) {
+        alert(err.message);
+      }
+    },
+  },
 };
 </script>
