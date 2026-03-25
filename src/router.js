32858864c9bf54b88a3cc392c951523f4e75b529
Musrh <musplay2023@gmail.com>
2026-03-25 23:49:01 +0100

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import PlanSelection from "./views/PlanSelection.vue";
 import Register from "./views/Register.vue";
 import Login from "./views/Login.vue";
