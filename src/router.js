432fbf5144c6bc2bae4171449184c536cb301f4a
Musrh <musplay2023@gmail.com>
2026-03-25 20:04:37 +0100

    Create router.js

---
+++ src/router.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from "vue-router";
+import PlanSelection from "../views/PlanSelection.vue";
+import Register from "../views/Register.vue";
+import Login from "../views/Login.vue";
+import Dashboard from "../views/Dashboard.vue";
+
+const routes = [
+  { path: "/", name: "PlanSelection", component: PlanSelection },
+  { path: "/register", name: "Register", component: Register },
+  { path: "/login", name: "Login", component: Login },
+  { path: "/dashboard", name: "Dashboard", component: Dashboard },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
