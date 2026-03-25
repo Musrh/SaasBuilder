e7d98bd9218e9fc2c3a2415253c1eac51ce584b2
Musrh <musplay2023@gmail.com>
2026-03-25 23:07:55 +0100

    Update Dashboard.vue

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,11 +1,37 @@
 <template>
-  <div>
+  <div class="container">
     <h1>Dashboard</h1>
+    <p>Plan actuel : {{ plan }}</p>
+    <p>Sections du projet : {{ sections.length }}</p>
   </div>
 </template>
 
 <script>
+import { auth, db } from "../firebase.js";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
+
 export default {
   name: "Dashboard",
+  data() {
+    return {
+      plan: "",
+      sections: [],
+    };
+  },
+  async created() {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const docRef = doc(db, "users", user.uid);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          this.plan = docSnap.data().plan;
+          this.sections = docSnap.data().project.sections || [];
+        }
+      } else {
+        this.$router.push("/login");
+      }
+    });
+  },
 };
 </script>
