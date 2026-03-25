2d6ccd8a08445e9d3cf07e7f2042fdd5b3c123de
Musrh <musplay2023@gmail.com>
2026-03-25 23:05:50 +0100

    Update PlanSelection.vue

--- src/views/PlanSelection.vue
+++ src/views/PlanSelection.vue
@@ -1,11 +1,19 @@
 <template>
-  <div>
-    <h1>Plan Selection</h1>
+  <div class="container">
+    <h1>Choisir votre plan</h1>
+    <button @click="selectPlan('free')">Gratuit - 30 jours</button>
+    <button @click="selectPlan('paid')">Payant - 5$ / mois</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "PlanSelection",
+  methods: {
+    selectPlan(plan) {
+      localStorage.setItem("selectedPlan", plan);
+      this.$router.push("/register");
+    },
+  },
 };
 </script>
