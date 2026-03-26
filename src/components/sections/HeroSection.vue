8b990f2f751308598eb954147a23766f6bdfd7dc
Musrh <musplay2023@gmail.com>
2026-03-26 20:13:29 +0100

    Create HeroSection.vue

---
+++ src/components/sections/HeroSection.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="p-10 bg-blue-500 text-white text-center">
+    <h1 class="text-3xl font-bold">{{ title }}</h1>
+    <p>{{ subtitle }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  title: String,
+  subtitle: String
+});
+</script>
