7f86475ba72691750cd100d7b9b3a41aff8af4cb
Musrh <musplay2023@gmail.com>
2026-03-26 04:22:26 +0100

    Create ComponentEditor.vue

---
+++ src/components/ComponentEditor.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="p-4 border rounded bg-gray-50 h-full">
+    <h3 class="font-bold mb-2">Éditeur de fichier</h3>
+
+    <div v-if="file">
+      <p class="text-sm text-gray-600 mb-2">Fichier sélectionné :</p>
+      <div class="bg-white p-2 border rounded text-sm">
+        {{ file.name }}
+      </div>
+    </div>
+
+    <div v-else class="text-gray-500">
+      Aucun fichier sélectionné
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  file: Object
+});
+</script>
