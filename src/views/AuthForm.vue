d5c39cce4ecc5f24ba7e5e4c04cef1e1a6f3b940
Musrh <musplay2023@gmail.com>
2026-03-26 19:28:05 +0100

    Update AuthForm.vue

--- src/views/AuthForm.vue
+++ src/views/AuthForm.vue
@@ -1,2 +1,43 @@
-<template><div>Auth OK</div></template>
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+
+import { auth, db } from "../firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+
+const email = ref("");
+const password = ref("");
+const isLogin = ref(true);
+
+const router = useRouter();
+
+const handleSubmit = async () => {
+  try {
+    if (isLogin.value) {
+      await signInWithEmailAndPassword(auth, email.value, password.value);
+    } else {
+      const cred = await createUserWithEmailAndPassword(
+        auth,
+        email.value,
+        password.value
+      );
+
+      await setDoc(doc(db, "users", cred.user.uid), {
+        email: email.value,
+        plan: localStorage.getItem("planChoisi") || "free",
+        createdAt: new Date(),
+        sections: []
+      });
+    }
+
+    router.push("/dashboard");
+
+  } catch (e) {
+    alert(e.message);
+  }
+};
+</script>
