44c93740c55344ae4eea974d712939c97a59be2e
Musrh <musplay2023@gmail.com>
2026-03-26 02:39:56 +0100

    Update Dashboard.vue

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,8 +2,7 @@
   <div class="min-h-screen bg-gray-100 p-6">
     <div class="max-w-4xl mx-auto bg-white p-8 rounded-xl shadow">
       <h1 class="text-3xl font-bold mb-4">Bienvenue sur votre Dashboard !</h1>
-      <p class="text-gray-700">Plan sélectionné : {{ planName }}</p>
-      <!-- Ici tu peux ajouter les composants / infos du Dashboard -->
+      <p class="text-gray-700">Plan choisi : {{ planName }}</p>
     </div>
   </div>
 </template>
@@ -14,7 +13,7 @@ import { useRoute } from "vue-router";
 const route = useRoute();
 const plan = route.query.plan || null;
 
-// Optionnel : afficher le nom complet du plan
+// Affichage du nom du plan
 const planMap = { 1: "Plan Offert", 2: "Plan Pro", 3: "Plan Premium" };
 const planName = planMap[plan] || "Non défini";
 </script>
