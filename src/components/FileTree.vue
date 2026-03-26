e97c1ce1ca3fac0d7411e1223a39575d10c0e2d9
Musrh <musplay2023@gmail.com>
2026-03-26 04:13:16 +0100

    Create FileTree.vue

---
+++ src/components/FileTree.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="p-4 border rounded bg-gray-50">
+    <h3 class="font-bold mb-2">Arborescence</h3>
+    <ul>
+      <li v-for="file in files" :key="file.name">
+        📄 {{ file.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  files: {
+    type: Array,
+    default: () => []
+  }
+});
+</script>
