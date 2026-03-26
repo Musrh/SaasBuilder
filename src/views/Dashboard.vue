a93fbd278d381b9b8dfae4e2f08ef0dc9d3bb3e6
Musrh <musplay2023@gmail.com>
2026-03-26 19:31:19 +0100

    Update Dashboard.vue

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,2 +1,35 @@
-<template><div>Dashboard OK</div></template>
-<script setup></script>
+<template>
+  <div class="p-6">
+    <h1>Dashboard</h1>
+
+    <div v-if="!user">Loading...</div>
+
+    <div v-else>
+      <p>{{ user.email }}</p>
+      <p>{{ user.plan }}</p>
+
+      <button @click="goBuilder">Builder</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import { auth, db } from "../firebase";
+import { doc, getDoc } from "firebase/firestore";
+
+const user = ref(null);
+
+onMounted(() => {
+  auth.onAuthStateChanged(async (u) => {
+    if (!u) return;
+
+    const snap = await getDoc(doc(db, "users", u.uid));
+    user.value = snap.data();
+  });
+});
+
+const goBuilder = () => {
+  window.location.href = "#/builder";
+};
+</script>
