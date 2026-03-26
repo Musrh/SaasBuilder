eea5f2c96bb5b998456182d00732e556687ca8a6
Musrh <musplay2023@gmail.com>
2026-03-26 03:58:53 +0100

    Update router.js

--- src/router.js
+++ src/router.js
@@ -3,10 +3,14 @@ import PlanSelection from "./views/PlanSelection.vue";
 import AuthForm from "./views/AuthForm.vue";
 import Dashboard from "./views/Dashboard.vue";
 
+import Builder from "./views/Builder.vue"; 
+
 const routes = [
   { path: "/", name: "PlanSelection", component: PlanSelection },
   { path: "/auth", name: "AuthForm", component: AuthForm },
   { path: "/dashboard", name: "Dashboard", component: Dashboard },
+
+{ path: "/dashboard", name: "Dashboard", component: Dashboard }
 ];
 
 const router = createRouter({
