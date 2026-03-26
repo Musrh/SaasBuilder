3479627c93ec4515ecee58c54ef807a40158cee9
Musrh <musplay2023@gmail.com>
2026-03-26 17:26:10 +0100

    Update router.js

--- src/router.js
+++ src/router.js
@@ -8,7 +8,7 @@ import Builder from './views/Builder.vue';
 const routes = [
   { path: '/', redirect: '/plan' },
   { path: '/plan', name: 'Plan', component: PlanSelection },
-  { path: '/register', name: 'AuthForm', component: AuthForm },
+  { path: '/auth', name: 'AuthForm', component: AuthForm },
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
   { path: '/builder', name: 'Builder', component: Builder },
 ];
