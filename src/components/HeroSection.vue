203dded188f8c11b4ad8cacabc62fe22c0244245
Musrh <musplay2023@gmail.com>
2026-03-26 20:01:04 +0100

    Update HeroSection.vue

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,1 +1,14 @@
 
+<template>
+  <div class="p-10 bg-blue-500 text-white text-center">
+    <h1 class="text-3xl font-bold">{{ title }}</h1>
+    <p>{{ subtitle }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  title: String,
+  subtitle: String
+});
+</script>
