50c24dae9a16ebddf3feb94304c9bb26eb1cbfaf
Musrh <musplay2023@gmail.com>
2026-03-27 11:19:57 +0100

    Create LogoSection.vue

---
+++ src/components/sections/LogoSection.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex items-center">
+    <span class="text-xl font-bold text-white">
+      {{ text }}
+    </span>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  text: {
+    type: String,
+    default: "MonSite"
+  }
+});
+</script>
