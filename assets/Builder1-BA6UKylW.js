05c6d17e7d70d6a3be111f0f77d5d40a1147502e
Musrh <Musrh@users.noreply.github.com>
2026-04-05 22:21:54 +0000

    deploy: 639bac81e83b5cc8a38a150c0a97f40ade22ba6b

---
+++ assets/Builder1-BA6UKylW.js
@@ -0,0 +1,1 @@
+import{s as e,v as t}from"./runtime-core.esm-bundler-Cm0XUIEk.js";import{d as n}from"./index-Bn5P_5oR.js";var r={};function i(n,r){return t(),e(`h1`,null,`This plan for single page`)}var a=n(r,[[`render`,i]]);export{a as default};
