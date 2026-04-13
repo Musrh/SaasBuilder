a01ea87bc6b307e07b2718777fd377f45734ba1c
Musrh <musplay2023@gmail.com>
2026-04-13 03:15:52 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <router-view />
+  <footer class="py-6 text-center border-t mt-10">
+    <router-link to="/privacy-policy" class="text-gray-500 text-sm hover:underline">
+      Privacy Policy
+    </router-link>
+  </footer>
 </template>
 
 <script>
