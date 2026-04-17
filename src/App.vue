16c9027e8b7abefb16a0fed084c50db83d8bf166
Musrh <musplay2023@gmail.com>
2026-04-17 20:14:50 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,21 @@
     <router-link to="/privacy-policy" class="text-gray-500 text-sm hover:underline">
       Privacy Policy
     </router-link>
+  
+<router-link to="/remboursement" class="text-gray-500 text-sm hover:underline">
+      Remboursement 
+    </router-link>  
+<router-link to="/confidentialite" class="text-gray-500 text-sm hover:underline">
+      Confidentialité
+    </router-link>
+  
+ <router-link to="/mentions" class="text-gray-500 text-sm hover:underline">
+      Mentions légales 
+    </router-link> 
+  <router-link to="/conditions" class="text-gray-500 text-sm hover:underline">
+      Conditions
+    </router-link>
+  
   </footer>
     </div>
 </template>
