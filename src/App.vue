3fe8d253604247b8194d3805590bc8d528f55d64
Musrh <musplay2023@gmail.com>
2026-04-19 21:28:24 +0100

    Remove footer section from App.vue
    Removed the footer section containing various links.

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,7 @@
 <template>
   <router-view />
   
-  <section>
-  <footer class="py-6 text-center border-t mt-10">
-    <div class="container">
-    <router-link to="/privacy-policy" class="text-gray-500 text-sm hover:underline">
-      Privacy Policy
-    </router-link>
-      </div>
- <div class="container"> 
-<router-link to="/remboursement" class="text-gray-500 text-sm hover:underline">
-      Remboursement 
-    </router-link>  
-   </div>
-    <div class="container">
-<router-link to="/confidentialite" class="text-gray-500 text-sm hover:underline">
-      Confidentialité
-    </router-link>
-  </div>
-    <div class="container">
- <router-link to="/mentions" class="text-gray-500 text-sm hover:underline">
-      Mentions légales 
-    </router-link> 
-      </div>
-    <div class="container">
-  <router-link to="/conditions" class="text-gray-500 text-sm hover:underline">
-      Conditions
-    </router-link>
-  </div>
-  </footer>
-    </section>
+  
     
 </template>
 
