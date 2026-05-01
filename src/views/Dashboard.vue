b27ae26e03032f124a2fa8176be55aea599f096a
Musrh <musplay2023@gmail.com>
2026-05-01 12:04:00 +0100

    Update links to use mronlinestores.com

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -288,11 +288,11 @@
               <span class="db-published-dot"></span>
               <span>Publié sur </span>
               <a
-                :href="`https://musrh.github.io/SaasBuilder/#/site/${userData.publishedSlug}`"
+                :href="`https://mronlinestores.com/${userData.publishedSlug}`"
                 target="_blank"
                 class="db-published-link"
               >
-                /site/{{ userData.publishedSlug }}
+                {{ userData.publishedSlug }}
               </a>
             </div>
           </div>
@@ -343,7 +343,7 @@
             <span class="db-action-icon">🌐</span>
             <div>
               <p class="db-action-title">Voir le store</p>
-              <p class="db-action-desc">/site/{{ userData.publishedSlug }}</p>
+              <p class="db-action-desc">mronlinestores.com/{{ userData.publishedSlug }}</p>
             </div>
           </button>
 
@@ -603,13 +603,13 @@ const toggleOrderDetail = (id) => {
 }
 
 const openStore = () => {
-  window.open(`https://musrh.github.io/SaasBuilder/#/site/${userData.value.publishedSlug}`, "_blank")
+  window.open(`https://mronlinestores.com/${userData.value.publishedSlug}`, "_blank")
 }
 
 const goToBuilder = async () => {
   // Vérifier si le slug est configuré
   if (userData.value?.publishedSlug) {
-    window.location.href = "https://musrh.github.io/SaasBuilder/#/saasgenerator"
+    window.location.href = "https://mronlinestores.com/#/saasgenerator"
   } else {
     router.push("/slug-setup")
   }
